bedad7b1603fa39e6b92bbf69501819662485378
Rin Arakaki <rnarkkx@gmail.com>
2023-03-12 12:26:17 +0900

    Update constants.rs

--- src/constants.rs
+++ src/constants.rs
@@ -8,18 +8,24 @@ use crate::repr::Repr;
 /// `\0`
 #[unconst]
 pub const NUL: Repr<char> = Repr::from('\0');
-/// `\t`
+/// `\a`, `\x07`
+#[unconst]
+pub const BEL: Repr<char> = Repr::from('\u{0007}');
+/// `\t`, `\x09`
 #[unconst]
 pub const HT: Repr<char> = Repr::from('\t');
-/// `\n`
+/// `\n`, `\x0A`
 #[unconst]
 pub const LF: Repr<char> = Repr::from('\n');
-/// `\v`
+/// `\v`, `\x0B`
 #[unconst]
 pub const VT: Repr<char> = Repr::from('\u{000B}');
-/// `\r`
+/// `\f`, `\x0C`
+#[unconst]
+pub const FF: Repr<char> = Repr::from('\u{000C}');
+/// `\r`, `\x0D`
 #[unconst]
 pub const CR: Repr<char> = Repr::from('\r');
-/// ` `
+/// ` `, `\x20`
 #[unconst]
 pub const SP: Repr<char> = Repr::from(' ');
