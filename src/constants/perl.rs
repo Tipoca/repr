a6d776600bb5ba6bd7f731d83bc76987b758d11a
Rin Arakaki <rnarkkx@gmail.com>
2023-03-12 12:18:10 +0900

    Update perl.rs

---
+++ src/constants/perl.rs
@@ -0,0 +1,13 @@
+use unconst::unconst;
+
+use crate::repr::Repr;
+
+#[unconst]
+/// `\d`
+pub const DIGIT: Repr<char> = Repr::from('0'..'9');
+#[unconst]
+/// `\s`
+pub const SPACE: Repr<char> = Repr::;
+#[unconst]
+/// `\w`
+pub const WORD: Repr<char> = Repr::from('A'..'Z') | ('a'..'z');
