f508eb04d2f8abfb7406b537a0a32f6ad7d21e57
Rin Arakaki <rnarkkx@gmail.com>
2023-03-16 02:55:19 +0900

    Update traits.rs

--- src/traits.rs
+++ src/traits.rs
@@ -1,1 +1,17 @@
-
+/// - `Copy` + `Clone`: possibility of `!` exponentiation
+/// - `PartialEq` + `Eq`: decidability
+#[unconst]
+#[const_trait]
+pub trait Integral: Copy + ~const Clone
+                    + ~const PartialEq + Eq
+                    + ~const PartialOrd + ~const Ord
+                    + Step
+                    + ~const Destruct
+                    + Debug
+                    + Sync + Send + RefUnwindSafe + UnwindSafe
+{
+    const MIN: Self;
+    const MAX: Self;
+    fn succ(self) -> Self;
+    fn pred(self) -> Self;
+}
