51dd4c514f240d18fd141d33e52e476d7cecd5f9
root <rnarkkx@gmail.com>
2023-03-18 10:09:37 +0000

    Update

--- experiments/rfc3987.rs
+++ experiments/rfc3987.rs
@@ -1,29 +1,38 @@
 //! <https://www.rfc-editor.org/rfc/rfc3987>
 
+use unconst::unconst;
+
 pub use repr::{
     Repr,
     constants::{EMPTY, DIGIT, SPACE, WORD as ALPHA},
 };
 
-pub const SUB_DELIMS: Pat
-    = EMPTY & '!' | '$' | '&' | '\'' | '(' | ')' | '*' | '+' | ',' | ';' | '=';
-pub const PCT_ENCODED: Pat = '%' & HEXDIG & HEXDIG;
-// pub const UCSCHAR: Pat
+#[unconst]
+pub const SUB_DELIMS: Repr<char>
+    = EMPTY * '!' | '$' | '&' | '\'' | '(' | ')' | '*' | '+' | ',' | ';' | '=';
+#[unconst]
+pub const PCT_ENCODED: Repr<char> = '%' * HEXDIG * HEXDIG;
+// pub const UCSCHAR: Repr<char>
 //     = %xA0..D7FF | %xF900..FDCF | %xFDF0..FFEF
 //     | %x10000..1FFFD | %x20000..2FFFD | %x30000..3FFFD
 //     | %x40000..4FFFD | %x50000..5FFFD | %x60000..6FFFD
 //     | %x70000..7FFFD | %x80000..8FFFD | %x90000..9FFFD
 //     | %xA0000..AFFFD | %xB0000..BFFFD | %xC0000..CFFFD
 //     | %xD0000..DFFFD | %xE1000..EFFFD;
-pub const UNRESERVED: Pat
+#[unconst]
+pub const UNRESERVED: Repr<char>
     = ALPHA | DIGIT | '-' | '.' | '_' | '~' | UCSCHAR;
-pub const USERINFO: Pat = (UNRESERVED | PCT_ENCODED | SUB_DELIMS | ':') * ..;
-pub const AUTHORITY: Pat = (USERINFO & '@')? & ihost & (':' & port);
-pub const HIER_PART: Pat
-    = "//" & AUTHORITY & ipath_abempty
+#[unconst]
+pub const USERINFO: Repr<char>
+    = (UNRESERVED | PCT_ENCODED | SUB_DELIMS | ':').exp();
+#[unconst]
+pub const AUTHORITY: Repr<char> = (USERINFO * '@')? * ihost * (':' * port);
+#[unconst]
+pub const HIER_PART: Repr<char>
+    = "//" * AUTHORITY * ipath_abempty
     | ipath_absolute
     | ipath_rootless
     | ipath_empty;
-pub const SCHEME: Pat = ALPHA & (ALPHA | DIGIT | '+' | '-' | '.') * ..;
-pub const IRI: Pat
-    = SCHEME & ':' & HIER_PART & ('?' & iquery)? & ('#' & ifragment)?;
+pub const SCHEME: Repr<char> = ALPHA * (ALPHA | DIGIT | '+' | '-' | '.').exp();
+pub const IRI: Repr<char>
+    = SCHEME * ':' * HIER_PART * ('?' * iquery)? * ('#' * ifragment)?;
