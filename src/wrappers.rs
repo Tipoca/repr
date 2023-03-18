585b16cd6f386574b1df1a895930d69927ca5644
root <rnarkkx@gmail.com>
2023-03-18 10:28:40 +0000

    Update

--- src/wrappers.rs
+++ src/wrappers.rs
@@ -1,3 +1,9 @@
-pub const fn empty() {
-    
+use unconst::unconst;
+
+use crate::repr::Repr;
+use crate::traits::Integral;
+
+#[unconst]
+pub const fn one<I: ~const Integral>(i: I) -> Repr<I> {
+    Repr::one(i)
 }
