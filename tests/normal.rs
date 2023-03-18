0939c6e0b8e257a19749367b4b6b798de7500bb7
root <rnarkkx@gmail.com>
2023-03-18 10:59:44 +0000

    Update

--- tests/normal.rs
+++ tests/normal.rs
@@ -6,23 +6,23 @@ fn reflexivity() {
 }
 
 #[test]
+fn mul_unit() {
+    assert_eq!(one('a').mul(seq([])), one('a'));
+    assert_eq!(seq([]).mul(one('a')), one('a'));
+}
+
+#[test]
 fn mul_linearity() {
     assert_eq!(one('a').mul(one('b')), seq(['a', 'b']));
 }
 
 #[test]
 fn or_associativity() {
-    assert_eq!(one('a').or(one('b').or(one('c'))), one('a').or(one('b')).or(one('c')));
+    assert_eq!(one('a').or(one('b').or(one('c'))),
+               one('a').or(one('b')).or(one('c')));
 }
 
 #[test]
 fn or_idempotence() {
     assert_eq!(one('a').or(one('a')), one('a'));
 }
-
-/*
-#[test]
-fn mul_unit() {
-    assert_eq!();
-}
-*/
