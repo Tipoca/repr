589bb58be7b49ebcfc2360261d4811efd13fe6f0
Rin Arakaki <rnarkkx@gmail.com>
2023-03-18 19:24:23 +0900

    Update normal.rs

--- tests/normal.rs
+++ tests/normal.rs
@@ -1,6 +1,16 @@
-use repr::Repr;
+use repr::{
+    Repr,
+    wrappers::one
+};
 
+#[test]
+fn or_associativity() {
+    assert_eq!(one('a').or(one('b').or(one('c'))), one('a').or(one('b')).or(one('c')));
+}
+
+/*
 #[test]
 fn mul_unit() {
     assert_eq!();
 }
+*/
