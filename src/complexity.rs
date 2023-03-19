b9b8af6f7f0dc863ac20556158209e5187390549
Rin Arakaki <rnarkkx@gmail.com>
2023-03-19 12:04:36 +0900

    Create

---
+++ src/complexity.rs
@@ -0,0 +1,12 @@
+//! <https://en.wikipedia.org/wiki/Star_height>
+
+use unconst::unconst;
+
+use crate::repr::Repr;
+use crate::traits::Integral;
+
+#[unconst]
+pub const fn hei<I: ~const Integral>(repr: &Repr<I>) -> usize {
+    match self {
+    }
+}
