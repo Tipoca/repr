23296aeb35e595798801d8093026d11c72926c13
Rin Arakaki <rnarkkx@gmail.com>
2023-03-21 21:01:46 +0900

    Update debug.rs

--- tests/debug.rs
+++ tests/debug.rs
@@ -11,4 +11,5 @@ fn debug() {
     debug!(one('a'), "One(Seq(['a']))");
     debug!(one('a').mul(one('b')), "One(Seq(['a', 'b']))");
     debug!(one('a').or(one('b')), "Or(One(Seq(['a'])), One(Seq(['b'])))");
+    debug!(one('a').inf(), "Inf(One(Seq(['a'])))");
 }
