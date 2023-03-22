df8c519c2adc39eed90b5c07ecc2786e5cb7a9e6
root <rnarkkx@gmail.com>
2023-03-22 09:19:15 +0000

    Update

--- tests/order.rs
+++ tests/order.rs
@@ -2,11 +2,11 @@ use repr::wrappers::one;
 
 #[test]
 fn inclusion() {
-    assert!(one('a').le(one('a').or(one('b')));
-    assert!(one('b').le(one('a').or(one('b')));
+    assert!(one('a').le(&one('a').or(one('b'))));
+    assert!(one('b').le(&one('a').or(one('b'))));
 }
 
 #[test]
 fn le_reflexivity() {
-    assert!(one('a').le(one('a'));
+    assert!(one('a').le(&one('a')));
 }
