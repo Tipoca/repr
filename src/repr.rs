7075d276121e2113d781ea7b4b5927375632893f
Rin Arakaki <rnarkkx@gmail.com>
2023-03-22 14:12:31 +0900

    Update repr.rs

--- src/repr.rs
+++ src/repr.rs
@@ -139,7 +139,7 @@ impl<I: ~const Integral> Repr<I> {
         }
     }
 
-    /// 𝜕, endofunctor
+    /// 𝜕, linear endofunctor
     pub const fn der(self, seq: Seq<I>) -> Self {
         match self {
             One(_seq) => unimplemented!(),
