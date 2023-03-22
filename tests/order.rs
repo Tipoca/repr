2d25e8a8079bc3b79fcbba14a2c0658f10bc2bf2
Rin Arakaki <rnarkkx@gmail.com>
2023-03-22 16:19:40 +0900

    Create order.rs

---
+++ tests/order.rs
@@ -0,0 +1,12 @@
+use repr::wrappers::one;
+
+#[test]
+fn inclusion() {
+    assert!(one('a').le(one('a').or(one('b')));
+    assert!(one('b').le(one('a').or(one('b')));
+}
+
+#[test]
+fn le_reflexivity() {
+    assert!(one('a').le(one('a'));
+}
