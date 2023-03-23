feb017ebba53435a91a59b8c182076f6c3fc357e
Rin Arakaki <rnarkkx@gmail.com>
2023-03-23 15:20:54 +0900

    Update functor.rs

--- src/functor.rs
+++ src/functor.rs
@@ -2,7 +2,7 @@ trait Functor {
     type A;
     type Out<B>: Functor;
 
-    fn fmap<F, To>(self, f: F) -> Self::Out<B>
+    fn fmap<F, B>(self, f: F) -> Self::Out<B>
     where
         F: FnOnce(Self::A) -> B;
 }
