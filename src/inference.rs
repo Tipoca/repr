fb38edd16a5c4b434295f37c2a5f437fab674c88
Rin Arakaki <rnarkkx@gmail.com>
2023-03-23 15:11:04 +0900

    Update inference.rs

--- src/inference.rs
+++ src/inference.rs
@@ -4,7 +4,7 @@ use crate::repr::Repr;
 use crate::traits::Integral;
 
 #[unconst]
-pub struct Is<I: ~const Integral>([I], Repr<I>);
+pub struct In<I: ~const Integral>([I], Repr<I>);
   
 match self {
     (i, One(seq)) if i.is(seq) => ,
